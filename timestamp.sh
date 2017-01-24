d885812b8f5c2158bc427b7654667d66645d3e8e
krafts <varun.net@gmail.com>
2017-01-24 16:08:00 -0500

    fixing timezone data
    it was not completely ISO-8601 compatible. date uses
    strftime/strptime %z which implements RFC-822
    (https://tools.ietf.org/html/rfc822#section-5.1) but RFC-822 does not
    correctly implement ISO-8601 extended date time format, in which timezone
    data has a colon in the middle indicating the correct time zone,
    instead it is partial extended format with date, time and partial basic
    format with timezone data being a 4 digit number without a colon.
    
    correct implementation of the extended format has colon in the timezone
    data (the four digit number).
    
    more info on ISO-8601:
    http://lists.ebxml.org/archives/ebxml-core/200104/pdf00005.pdf
    
    git log time format in ISO-8601 implements this correctly.
    https://www.git-scm.com/docs/git-log#_pretty_formats (%cI - strict
    ISO-8601 format).

--- timestamp.sh
+++ timestamp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-date +'%Y-%m-%dT%H:%M:%SZ%z'
+date_data=$(date +"%Y-%m-%dT%H:%M:%S")
+tz_data=$(date +"%z" | sed 's/\([-+]\)\([0-9]\{2\}\)\(.*\)/\1\2:\3/g')
+
+echo "$date_data$tz_data"

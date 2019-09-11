c6b150aaa5da51cf33b8635ec6bcfb4ab565ed6e
krafts <varun.net@gmail.com>
2019-09-11 14:03:23 -0400

    timestamp: adding option for utc.

--- timestamp.sh
+++ timestamp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-date_data=$(date +"%Y-%m-%dT%H:%M:%S")
-tz_data=$(date +"%z" | sed 's/\([-+]\)\([0-9]\{2\}\)\(.*\)/\1\2:\3/g')
+if [ "$1" == "-u" ]; then
+  utc="-u"
+else
+  utc=
+fi
+
+date_data=$(date $utc +"%Y-%m-%dT%H:%M:%S")
+tz_data=$(date $utc +"%z" | sed 's/\([-+]\)\([0-9]\{2\}\)\(.*\)/\1\2:\3/g')
 
 echo "$date_data$tz_data"

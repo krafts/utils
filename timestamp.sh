09ede2092c61601aa1c4ec4871572ca06ec9415d
krafts <varun.net@gmail.com>
2019-10-04 11:33:03 -0400

    timestamp: simplification. no more iso-8601 compliant. but less
    processing and removes one char.

--- timestamp.sh
+++ timestamp.sh
@@ -6,7 +6,7 @@ else
   utc=
 fi
 
-date_data=$(date $utc +"%Y-%m-%dT%H:%M:%S")
-tz_data=$(date $utc +"%z" | sed 's/\([-+]\)\([0-9]\{2\}\)\(.*\)/\1\2:\3/g')
-
-echo "$date_data$tz_data"
+#date_data=$(date $utc +"%Y-%m-%dT%H:%M:%S")
+#tz_data=$(date $utc +"%z" | sed 's/\([-+]\)\([0-9]\{2\}\)\(.*\)/\1\2:\3/g')
+date_tz=$(date $utc +"%Y-%m-%dT%H:%M:%S%z")
+echo "$date_tz"

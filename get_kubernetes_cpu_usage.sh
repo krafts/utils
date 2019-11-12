10a6ebc53906f475653a2613272aacf3f78ebc07
krafts <varun.net@gmail.com>
2019-11-12 11:46:25 -0500

    kubernetes: fixing namespace placement and optimizing cpu
    requests/limits query by using jsonpath instead of using jq.

--- get_kubernetes_cpu_usage.sh
+++ get_kubernetes_cpu_usage.sh
@@ -2,16 +2,15 @@
 
 NAMESPACE="${1}"
 
-if [ -z ${NAMESPACE} ]; then
-  kubectl_command="kubectl --all-namespaces"
+if [ -z "${NAMESPACE}" ]; then
+  namespace="--all-namespaces"
 else
-  kubectl_command="kubectl -n ${NAMESPACE}"
+  namespace="-n ${NAMESPACE}"
 fi
 
-requests=$(${kubectl_command} get pods -o json | jq '.items[] | .spec.containers[].resources.requests.cpu' | grep -v null | sed 's/"//g'| while read i; do if [[ $i == *m ]]; then i=${i%%m}; i=$(awk "BEGIN {printf \"%.2f\",${i}/1000}"); fi; echo $i; done | awk '{SUM+=$1} END {print SUM}')
-limits=$(${kubectl_command} get pods -o json | jq '.items[] | .spec.containers[].resources.requests.cpu' | grep -v null | sed 's/"//g'| while read i; do if [[ $i == *m ]]; then i=${i%%m}; i=$(awk "BEGIN {printf \"%.2f\",${i}/1000}"); fi; echo $i; done | awk '{SUM+=$1} END {print SUM}')
-top=$(${kubectl_command} top pods --no-headers | awk '{print $2}' | while read i; do if [[ $i == *m ]]; then i=${i%%m}; i=$(awk "BEGIN {printf \"%.2f\",${i}/1000}"); fi; echo $i; done | awk '{SUM+=$1} END {print SUM}')
-
+requests=$(kubectl get pods ${namespace} -o=jsonpath='{.items[*].spec.containers[*].resources.requests.cpu}' | xargs -n 1 | while read i; do if [[ $i == *m ]]; then i=${i%%m}; i=$(awk "BEGIN {printf \"%.2f\",${i}/1000}"); fi; echo $i; done | awk '{SUM+=$1} END {print SUM}')
+limits=$(kubectl get pods ${namespace} -o=jsonpath='{.items[*].spec.containers[*].resources.limits.cpu}' | xargs -n 1 | while read i; do if [[ $i == *m ]]; then i=${i%%m}; i=$(awk "BEGIN {printf \"%.2f\",${i}/1000}"); fi; echo $i; done | awk '{SUM+=$1} END {print SUM}')
+top=$(kubectl top pods --no-headers ${namespace}| awk '{print $2}' | while read i; do if [[ $i == *m ]]; then i=${i%%m}; i=$(awk "BEGIN {printf \"%.2f\",${i}/1000}"); fi; echo $i; done | awk '{SUM+=$1} END {print SUM}')
 
 echo "requests: ${requests}"
 echo "limits: ${limits}"

b98b15f5873e704ad7ae2802d7d9db1d694e85f2
krafts <varun.net@gmail.com>
2020-02-10 16:59:42 -0500

    github: merge pr: check to see if the branch needs to be updated, if so
    then update the branch.

--- github_merge_pr.sh
+++ github_merge_pr.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 
-PR_NUMBER="${1}"
+set -ue
+
+PULL_NUMBER="${1}"
 ACCOUNT="${2:-ACCOUNT}"
 REPO="${3:-REPO}"
 GITHUB_TOKEN=$(cat ~/.github_git_alias_auth_token)
-PR_URL="https://api.github.com/repos/${ACCOUNT}/${REPO}/pulls/${PR_NUMBER}"
+GITHUB_API_URL="https://api.github.com/repos/${ACCOUNT}/${REPO}"
+PR_URL="${GITHUB_API_URL}/pulls/${PULL_NUMBER}"
 
 while true; do
-    ## TODO figure out auto merge/rebase
-    # curl -X PUT \
-    #     -H "Authorization: token ${GITHUB_TOKEN}" \
-    #     -H "Content-Type: application/json" \
-    #     "${PR_URL}/update-branch"
+    ## check if the branch is outdated. if it is then update the branch.
+    master_sha=$(curl -s \
+        -H "Authorization: token ${GITHUB_TOKEN}" \
+        -H "Accept: application/vnd.github.VERSION.sha" \
+        "${GITHUB_API_URL}/commits/master")
 
+    ## this returns only 250 commits on the branch/PR, if more than 250 commits,
+    ## then need to use the commits api.
+    if ! curl -s \
+        -H "Authorization: token ${GITHUB_TOKEN}" \
+        "${PR_URL}/commits" \
+    | grep '"sha"' \
+    | awk -F: '{print $2}' \
+    | sed -e 's/[", ]//g' \
+    | grep -q "${master_sha}"; then
+        echo "updating branch, merging master into the branch..."
+        curl -X PUT \
+            -H "Authorization: token ${GITHUB_TOKEN}" \
+            -H "Accept: application/vnd.github.lydian-preview+json"\
+            "${PR_URL}/update-branch"
+    fi
+
+    ## merge
     status_code=$(curl -o /dev/null -s -w "%{http_code}" -X PUT \
         -H "Authorization: token ${GITHUB_TOKEN}" \
         -H "Content-Type: application/json" \
         --data '{"merge_method": "squash"}' \
         "${PR_URL}/merge")
-
     case ${status_code} in
         200) echo "merged!"; break;;
-        405) echo "cannot merge... check for approvals or update branch";;
+        405) echo "cannot merge... check for approvals or waiting on checks to complete...";;
         409) echo "cannot merge... CONFLICT!!!";;
         *) echo "something went wrong with the merge";;
     esac

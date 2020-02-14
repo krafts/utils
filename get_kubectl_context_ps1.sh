e2e8ccf2eeb48b2eb35622d1acb89072bd94847d
krafts <varun.net@gmail.com>
2020-02-14 18:19:43 -0500

    ps1: k8s context: after trying few different ways to color the prompts
    running into issues with the cursor when used with __git_ps1, finally
    settling with just plain text chars.

--- get_kubectl_context_ps1.sh
+++ get_kubectl_context_ps1.sh
@@ -24,25 +24,58 @@ bold="\e[1m"
 blink="\e[5m"
 clear_all="\e[0m"
 
-if [ "$environment" == "dabl-prod" ]; then
-  bg="${bg_red}"
-  fg="${fg_black}${bold}${blink}"
+## CLEAR
+CLEAR_ALL='\[\033[00m\]'
+
+## BACKGROUNDS
+BG_RED='\[\033[41m\]'
+BG_GREEN='\[\033[42m\]'
+BG_YELLOW='\[\033[43m\]'
+BG_BLUE='\[\033[44m\]'
+BG_PURPLE='\[\033[45m\]'
+BG_CYAN='\[\033[46m\]'
+BG_WHITE='\[\033[47m\]'
+
+## FOREGROUNDS
+FG_BLACK='\[\033[1;30m\]'
+FG_BLUE='\[\033[1;34m\]'
+FG_GREEN='\[\033[1;32m\]'
+FG_CYAN='\[\033[1;36m\]'
+FG_RED='\[\033[1;31m\]'
+FG_PURPLE='\[\033[1;35m\]'
+FG_BROWN='\[\033[1;33m\]'
+FG_YELLOW='\[\033[1;33m\]'
+
+source ~/.kubectl_context_ps1_domains
+
+if [ "$environment" == "${PROD}" ]; then
+  BG="${BG_RED}"
+  FG="${FG_BLACK}"
+  extra_chars_b="PROD->>!!! "
+  extra_chars_e=" !!!<<-PROD"
 fi
 
-if [ "$environment" == "dabl-staging" ]; then
-  bg="${bg_yellow}"
-  fg="${fg_black}${bold}"
+if [ "$environment" == "${STAGING}" ]; then
+  BG="${BG_YELLOW}"
+  FG="${FG_BLACK}"
+  extra_chars_b="STAGING "
+  extra_chars_e=" STAGING"
 fi
 
-if [ "$environment" == "dabl-dev" ]; then
+if [ "$environment" == "${DEV}" ]; then
   u=$(echo ${cluster} |  awk -F- '{print $1}')
   if [ "${u}" == "${USER}" ]; then
     exit 0
   fi
 
-  bg="${bg_purple}"
-  fg="${fg_black}${bold}"
+  BG="${BG_PURPLE}"
+  FG="${FG_BLACK}"
+  extra_chars_b="DEV "
+  extra_chars_e=" DEV"
 fi
 
 ## adding extra space at the end for PS1
-printf "${fg}${bg} ${cluster} ${clear_all} "
+##printf "${fg}${bg} ${cluster} ${clear_all}"
+##printf "${extra_chars_b}${cluster}${extra_chars_e}"
+#echo  "${BG}${FG} ${extra_chars_b}${cluster}${extra_chars_e} ${CLEAR_ALL}"
+printf "${extra_chars_b}${cluster}${extra_chars_e}"

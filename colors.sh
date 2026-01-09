b5536ad83806a684a2029da9e3e39e8a787b50d2
krafts <varun.net@gmail.com>
2026-01-08 21:15:53 -0500

    colors: separate out scripts colors with shell colors

--- colors.sh
+++ colors.sh
@@ -1,27 +1,13 @@
-#!/bin/bash
-#
-#   This file echoes a bunch of color codes to the
-#   terminal to demonstrate what's available.  Each
-#   line is the color code of one forground color,
-#   out of 17 (default + 16 escapes), followed by a
-#   test use of that color on all nine background
-#   colors (default + 8 escapes).
-#
 
-T='gYw'   # The test text
-
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
-
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-           '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-  echo -en " $FGs \033[$FG  $T  "
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
-  done
-  echo;
-done
-echo
-
+# Console Colors
+# Colors are taken from 8/16 Colors at https://misc.flogisoft.com/bash/tip_colors_and_formatting
+TXT_CLEAR="\033[0m"
+TXT_RED="\033[31m"
+TXT_GREEN="\033[32m"
+TXT_YELLOW="\033[33m"
+TXT_BLUE="\033[34m"
+TXT_MAGENTA="\033[35m"
+TXT_CYAN="\033[36m"
+TXT_LIGHT_GRAY="\033[37m"
+TXT_DARK_GRAY="\033[90m"
+TXT_WHITE="\033[97m"
